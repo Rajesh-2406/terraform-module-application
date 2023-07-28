ff5d5ea0a91793cbeae498a773ea039df0f3ca35
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-07-28 07:34:43 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
-resource  "null_resource" "test"  {
-  triggers = {
-    xyz = timestamp()
-  }
-  provisioner "local-exec"  {
-    command = "echo Hello World - Env - ${var.env}"
-  }
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:600222537277:parameter/roboshop.frontend.*"
+      }
+    ]
+  })
 }
