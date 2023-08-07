8389f2ee99dc8f6a633572dba725a4d1da9a8cf9
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-07 10:55:25 +0530

    terraform

--- main.tf
+++ main.tf
@@ -25,13 +25,13 @@ resource  "aws_iam_role"  "role"  {
   name = "${var.component}-${var.env}-ec2-role"
 
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    Version = "2012-10-17"
+    Statement = [
       {
-        Action  = "sts:AssumeRole"
-        Effect  = "Allow"
-        Sid = ""
-        principal = {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
           Service = "ec2.amazonaws.com"
         }
       },
