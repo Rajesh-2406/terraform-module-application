bae24f4aea94ce7aceac924b3650044286cb2def
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-26 19:34:44 +0530

    vpc subnet

--- main.tf
+++ main.tf
@@ -136,10 +136,13 @@ tags = {
 }
 }
 resource "aws_instance" "test" {
+
   ami = data.aws_ami.ami.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
   subnet_id = var.subnet_id
+
+
   tags = {
     Name = var.component
   }
