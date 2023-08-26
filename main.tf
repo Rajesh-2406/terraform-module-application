8af7e01c1afca56cbd6b28949a5912f94fb97739
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-26 19:43:28 +0530

    vpc subnet

--- main.tf
+++ main.tf
@@ -118,17 +118,17 @@ resource "aws_security_group" "sg" {
    name        = "${var.component}-${var.env}-sg"
    description = "${var.component}-${var.env}-sg"
    vpc_id = var.vpc_id
-ingress {
+  ingress {
    from_port   = 0
    to_port     = 0
    protocol    = "-1"
    cidr_blocks = ["0.0.0.0/0"]
 }
-egress {
-   from_port   = 0
-   to_port     = 0
-   protocol    = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
+   egress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
 
 }
 tags = {
